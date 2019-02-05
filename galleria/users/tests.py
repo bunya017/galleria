a05443b11f87a1f3235ea55d05dc5b4b2eb7446a
Bunyaminu Sani <dollabills007@gmail.com>
2019-02-05 15:19:33 +0100

    user registration test

--- galleria/users/tests.py
+++ galleria/users/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class UserRegistrationTest(APITestCase):
+	def setUp(self):
+		User.objects.create_user('testUser', 'testEmail@mail.com', 'testPassword')
+		self.url = reverse('user-registration')
+
+	def test_user_registration(self):
+		"""
+		Test user can register.
+		"""
+		data = {
+			'username': 'testUser1',
+			'email': 'testEmail.One@mail.com',
+			'password': 'testPassword'
+		}
+		response = self.client.post(self.url, data)
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+	
