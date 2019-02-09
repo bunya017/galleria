c7f4aa588e355e92960e621c898aefab6f83e5b4
Bunyaminu Sani <dollabills007@gmail.com>
2019-02-09 05:06:20 +0100

    Modified CatalogSerializer to return response based on request.user, catalog creation uses request.user as owner.

--- galleria/catalogs/views.py
+++ galleria/catalogs/views.py
@@ -15,3 +15,6 @@ class CatalogList(generics.ListCreateAPIView):
 		except TypeError:
 			raise NotAuthenticated
 		return queryset
+
+	def perform_create(self, serializer):
+		serializer.save(owner=self.request.user)
