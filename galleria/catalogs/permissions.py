f00684c20741d49fbedb79a894b845afed8bfcb7
unknown <Bunyaminu Sani>
2019-02-24 19:52:05 +0100

    Add IsProductEntryOwnerOrReadOnly permission

--- galleria/catalogs/permissions.py
+++ galleria/catalogs/permissions.py
@@ -6,7 +6,6 @@ from .models import Catalog
 class IsCatalogOwnerOrReadOnly(permissions.BasePermission):
 	"""
 	Object-level permission to only allow owners of a catalog to edit it.
-	Assumes the model instance has an `owner` attribute.
 	"""
 
 	def has_object_permission(self, request, view, obj):
@@ -21,7 +20,6 @@ class IsCatalogOwnerOrReadOnly(permissions.BasePermission):
 class IsCategoryOwnerOrReadOnly(permissions.BasePermission):
 	"""
 	Object-level permission to only allow owners of a category.catalog to edit it.
-	Assumes the model instance has an `owner` attribute.
 	"""
 
 	def has_permission(self, request, view):
@@ -40,3 +38,27 @@ class IsCategoryOwnerOrReadOnly(permissions.BasePermission):
 			return True
 
 		return obj.catalog.owner == request.user
+
+
+class IsProductEntryOwnerOrReadOnly(permissions.BasePermission):
+	"""
+	Object-level permission to only allow owners of a 
+	productentry.category.catalog to edit it.
+	"""
+
+	def has_permission(self, request, view):
+		# Read permissions are allowed to any request,
+		# so we'll always allow GET, HEAD or OPTIONS requests.
+		if request.method in permissions.SAFE_METHODS:
+			return True
+	
+		obj = Catalog.objects.get(slug=view.kwargs['category__catalog__slug'])
+		return obj.owner == request.user
+
+	def has_object_permission(self, request, view, obj):
+		# Read permissions are allowed to any request,
+		# so we'll always allow GET, HEAD or OPTIONS requests.
+		if request.method in permissions.SAFE_METHODS:
+			return True
+
+		return obj.category.catalog.owner == request.user
