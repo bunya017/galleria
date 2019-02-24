f96e8099e2060658ae7ecc93b27057f63ababcfc
unknown <Bunyaminu Sani>
2019-02-24 15:54:21 +0100

    Add IsCategoryOwnerOrReadOnly permission

--- galleria/catalogs/permissions.py
+++ galleria/catalogs/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import exceptions, permissions
+from .models import Catalog
 
 
 
@@ -15,3 +16,27 @@ class IsCatalogOwnerOrReadOnly(permissions.BasePermission):
 			return True
 
 		return obj.owner == request.user
+
+
+class IsCategoryOwnerOrReadOnly(permissions.BasePermission):
+	"""
+	Object-level permission to only allow owners of a category.catalog to edit it.
+	Assumes the model instance has an `owner` attribute.
+	"""
+
+	def has_permission(self, request, view):
+		# Read permissions are allowed to any request,
+		# so we'll always allow GET, HEAD or OPTIONS requests.
+		if request.method in permissions.SAFE_METHODS:
+			return True
+
+		obj = Catalog.objects.get(slug=view.kwargs['catalog__slug'])
+		return obj.owner == request.user
+
+	def has_object_permission(self, request, view, obj):
+		# Read permissions are allowed to any request,
+		# so we'll always allow GET, HEAD or OPTIONS requests.
+		if request.method in permissions.SAFE_METHODS:
+			return True
+
+		return obj.catalog.owner == request.user
