53c38c5ad6b7f83e068f6b89a63577676adab510
Bunyaminu Sani <dollabills007@gmail.com>
2019-03-02 19:27:05 +0100

    Add 'required' kwarg on email field of UserSerializer

--- galleria/users/serializers.py
+++ galleria/users/serializers.py
@@ -8,7 +8,10 @@ class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ('id', 'username', 'email', 'password')
-		extra_kwargs = {'password': {'write_only': True}}
+		extra_kwargs = {
+			'password': {'write_only': True},
+			'email': {'required': True}
+		}
 
 	def create(self, validated_data):
 		user = User(
