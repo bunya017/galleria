49512ce8a9950754b6ba021964fb44975cc26a53
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-03-14 14:03:10 +0100

    Added field 'slug' to Category model. 'slug' is generated automatically from category name.

--- galleria/catalogs/models.py
+++ galleria/catalogs/models.py
@@ -45,11 +45,16 @@ class Catalog(models.Model):
 class Category(models.Model):
 	name = models.CharField(max_length=150)
 	catalog = models.ForeignKey(Catalog, related_name='categories', on_delete=models.CASCADE)
+	slug = models.SlugField(unique=True)
 	created_on = models.DateTimeField(auto_now_add=True)
 	description = models.CharField(max_length=255, blank=True)
 
 	class Meta:
 		verbose_name_plural = 'Categories'
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return self.name
