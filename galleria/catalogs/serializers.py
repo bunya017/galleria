283abfb624d0fa433beaeb8a58996947be70649f
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-03-14 14:01:58 +0100

    Created serializers for Catalog, Category, ProductEntry and ProductImage models.

---
+++ galleria/catalogs/serializers.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Catalog, Category, ProductEntry, ProductImage
+
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = ProductImage
+		fields = ('id', 'product', 'title', 'photo')
+
+
+class ProductEntrySerializer(serializers.ModelSerializer):
+	photos = ProductImageSerializer(many=True)
+
+	class Meta:
+		model = ProductEntry
+		fields = (
+			'id', 'name', 'category', 'description', 'price', 'reference_number', 
+			'created_by', 'created_on', 'last_modified', 'photos',
+		)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+	product_enties = ProductEntrySerializer(many=True)
+
+	class Meta:
+		model = Category
+		fields = ('id', 'name','catalog','created_on','description', 'product_enties')
+
+
+class CatalogSerializer(models.ModelSerializer):
+	categories = CategorySerializer(many=True)
+
+	class Meta:
+		model = Catalog
+		fields = (
+			'name', 'created_on', 'description', 'contact_address', 'contact_email', 
+			'contact_phone', 'categories',
+		)
