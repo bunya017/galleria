ba60398cae037be7384148b1b1f84f1c5362bfae
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-03-14 13:39:35 +0100

    Added email validation while creating users

--- galleria/users/views.py
+++ galleria/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import generics, permissions, serializers, status
 from .serializers import UserSerializer
 
@@ -6,3 +7,12 @@ from .serializers import UserSerializer
 class UserRegistration(generics.CreateAPIView):
 	serializer_class = UserSerializer
 	permission_classes = (permissions.AllowAny,)
+
+	def perform_create(self, serializer):
+		data = serializer.validated_data
+		email = data['email']
+		if email == '':
+			raise serializers.ValidationError({'email': 'This field may not be blank.'})
+		elif User.objects.filter(email=email).exists():
+			raise serializers.ValidationError({'email': 'A user with that email already exists.'})
+		serializer.save()
