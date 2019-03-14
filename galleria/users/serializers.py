b8d513ae9ad7a06765249221bce9112a242b19bc
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-03-14 13:45:10 +0100

    Add UniqueValidator to UserSerializer email field

--- galleria/users/serializers.py
+++ galleria/users/serializers.py
@@ -1,9 +1,18 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 
 
 class UserSerializer(serializers.ModelSerializer):
+	email = serializers.EmailField(
+		validators=[
+			UniqueValidator(
+				queryset=User.objects.all(),
+				message='A user with that email already exists.'
+			)
+		]
+	)
 
 	class Meta:
 		model = User
