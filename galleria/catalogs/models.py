35555ba084d6c6642eb2c8fc1e8ca0c197ca7240
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-03-14 13:41:17 +0100

    Added field 'slug' to Catalog model. 'slug' is generated automatically from catalog name.

--- galleria/catalogs/models.py
+++ galleria/catalogs/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 
 
 
@@ -22,6 +23,7 @@ subscription_plan = models.CharField(
 class Catalog(models.Model):
 	owner = models.ForeignKey(User, on_delete=models.CASCADE)
 	name = models.CharField(max_length=150)
+	slug = models.SlugField(unique=True)
 	created_on = models.DateTimeField(auto_now_add=True)
 	description = models.CharField(max_length=255)
 	contact_address = models.CharField(max_length=255)
@@ -31,6 +33,10 @@ class Catalog(models.Model):
 	class Meta:
 		verbose_name = 'Catalog'
 		verbose_name_plural = 'Catalogs'
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Catalog, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return self.name
