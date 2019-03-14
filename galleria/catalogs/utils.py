d3a864761ee6365a53d2877fdcf9a619d455dd84
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-03-14 13:43:36 +0100

    Moved 'generate_photo()' from catalogs.tests.ProductImageListTest to catalogs.utils

--- galleria/catalogs/utils.py
+++ galleria/catalogs/utils.py
@@ -1,3 +1,5 @@
+import io
+from PIL import Image
 from datetime import datetime
 from hashids import Hashids
 
@@ -10,3 +12,12 @@ def generate_hash_id(id, salt='catalog', len=16):
 	hashids = Hashids(salt=timestamp+salt, min_length=len)
 	generated_hash = hashids.encode(id)
 	return generated_hash
+
+
+def generate_photo(filename):
+		file = io.BytesIO()
+		image = Image.new('RGBA', size=(640, 640), color=(155,0,0))
+		image.save(file, 'png')
+		file.name = '%s.png' % filename
+		file.seek(0)
+		return file
