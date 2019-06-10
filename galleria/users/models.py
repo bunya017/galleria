7f49e2705f659062d446313f9074a16e15f0bde9
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-06-10 21:59:38 +0100

    feat: Add UserProfile model and its migrations.

--- galleria/users/models.py
+++ galleria/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
@@ -10,3 +11,12 @@ from rest_framework.authtoken.models import Token
 def create_auth_token(sender, instance=None, created=False, **kwargs):
 	if created:
 		Token.objects.create(user=instance)
+
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	address = models.CharField(max_length=250)
+	phone = models.CharField(max_length=25)
+
+	def __str__(self): 
+		return self.user.username + ' (' + self.user.get_fullname() + ')'
