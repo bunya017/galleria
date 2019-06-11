6b0f422a3b23d5f15acccd88e0da2daab1ac40f7
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-06-11 06:59:25 +0100

    Add create_user_profile signal.

--- galleria/users/models.py
+++ galleria/users/models.py
@@ -13,6 +13,13 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
 		Token.objects.create(user=instance)
 
 
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance=None, created=False, **kwargs):
+	if created:
+		UserProfile.objects.create(user=instance)
+
+
+
 class UserProfile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	address = models.CharField(max_length=250)
