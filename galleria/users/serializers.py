c75d5fffd13a7d59ade76379d9211a222775edf0
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-06-11 07:24:59 +0100

    refactor: Add first_name & last_name to UserSreializer fields.

--- galleria/users/serializers.py
+++ galleria/users/serializers.py
@@ -17,10 +17,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = User
-		fields = ('id', 'username', 'email', 'password')
+		fields = ('id', 'username', 'email', 'first_name', 'last_name', 'password')
 		extra_kwargs = {
 			'password': {'write_only': True},
-			'email': {'required': True}
+			'email': {'required': True},
+			'first_name': {'read_only': True},
+			'last_name': {'read_only': True}
 		}
 
 	def create(self, validated_data):
