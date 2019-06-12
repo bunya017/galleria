9f514fe11af676f00ddde805de13e5421ada9d1b
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-06-12 15:59:40 +0100

    refactor: Add Collection to admin site.

--- galleria/catalogs/admin.py
+++ galleria/catalogs/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from .models import Catalog, Category, ProductEntry, ProductImage
+from .models import (
+	Catalog, Category, ProductEntry, ProductImage, Collection,
+	CollectionProduct
+)
+
+
+
+class CollectionProductInline(admin.TabularInline):
+	model = CollectionProduct
+	extra = 1
+
+
+class CollectionAdmin(admin.ModelAdmin):
+	inlines = [CollectionProductInline]
 
 
 admin.site.register(Catalog)
 admin.site.register(Category)
 admin.site.register(ProductEntry)
 admin.site.register(ProductImage)
+admin.site.register(Collection, CollectionAdmin)
