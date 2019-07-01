a7b6ab0277ce4a450cc34a8278eaac8f73fead4c
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-07-01 08:04:36 +0100

    refactor: Add Collection and CollectionProduct to django admin

--- galleria/catalogs/admin.py
+++ galleria/catalogs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import (
-	Catalog, Category, ProductEntry, ProductImage
+	Catalog, Category, ProductEntry, ProductImage,
+	Collection, CollectionProduct
 )
 
 
@@ -9,3 +10,5 @@ admin.site.register(Catalog)
 admin.site.register(Category)
 admin.site.register(ProductEntry)
 admin.site.register(ProductImage)
+admin.site.register(Collection)
+admin.site.register(CollectionProduct)
