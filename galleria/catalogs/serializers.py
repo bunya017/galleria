1ed3dc04084d3df1f985a1563664e9f6a4094840
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-07-09 12:50:00 +0100

    refactor: Add VersatileImageFieldSerializer.sizes in ProductImageSerializer.photo field

--- galleria/catalogs/serializers.py
+++ galleria/catalogs/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from versatileimagefield.serializers import VersatileImageFieldSerializer
 from .models import (
 	Catalog, Category, ProductEntry, ProductImage,
 	Collection, CollectionProduct
@@ -9,6 +10,14 @@ from . import relations
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
+	photo = VersatileImageFieldSerializer(
+		sizes=[
+			('full_size', 'url'),
+			('thumbnail', 'thumbnail__100x100'),
+			('small', 'thumbnail__480x480')
+		]
+	)
+
 	class Meta:
 		model = ProductImage
 		fields = ('id', 'product', 'photo')
