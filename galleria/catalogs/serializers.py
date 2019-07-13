05752c52b816155955d722a8c42da2244b1c6c7f
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-07-13 10:57:02 +0100

    refactor(CollectionSerializer): Remove depth

--- galleria/catalogs/serializers.py
+++ galleria/catalogs/serializers.py
@@ -122,7 +122,6 @@ class CollectionSerializer(serializers.ModelSerializer):
 			'background_image', 'collection_products'
 		)
 		extra_kwargs = {'slug': {'read_only': True}}
-		depth = 1
 
 
 class CategorySerializer(serializers.ModelSerializer):
