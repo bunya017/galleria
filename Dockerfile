9505923555aa52dca163368ff69f47b16cc25842
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-12-22 01:26:51 +0100

    deployFix: Move caprover related files to repo root

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN mkdir -p /usr/src/data
 WORKDIR /app
 COPY ./ /app
 
-RUN pip install -r requirements.txt
+RUN pip install -r galleria/requirements.txt
---
+++ prestart.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+# Run collect static and migrations
+python galleria/manage.py collectstatic --noinput
+python galleria/manage.py migrate
