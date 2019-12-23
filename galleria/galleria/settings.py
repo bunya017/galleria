4fa3266cf3254cd303d48cc2895be8ffddfa02f0
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-12-23 01:34:39 +0100

    feat(settings): Fetch ALLOWED_HOSTS from env

--- galleria/galleria/settings.py
+++ galleria/galleria/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['bunya017.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
